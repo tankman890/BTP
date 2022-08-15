d1484c854cf21affa52b244839632732f039b4f1
Sai Pranav Raj K <tankman890@Sais-MacBook-Air.local>
2022-08-16 01:03:32 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+
+const fileName = './data.json';
+
+app.post('/grant_access/:rfid', (req, res) => {
+    const file = require(fileName);
+    file.push(req.params.rfid);
+
+    fs.writeFile(fileName, JSON.stringify(file), (err) => {});
+
+    res.end();
+});
+
+app.put('/revoke_access/:rfid', (req, res) => {
+    var file = require(fileName);
+    file = file.filter((val) => { return val != req.params.rfid; })
+
+    fs.writeFile(fileName, JSON.stringify(file), (err) => {});
+
+    res.end();
+});
+
+app.get('/get_list', (req, res) => {
+    const file = require(fileName);
+    res.json(JSON.stringify(file));
+});
+
+const PORT = process.env.PORT || 5070;
+
+app.listen(PORT);
+
